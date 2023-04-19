ee031957a28fd629b5a3a07e7a40e96ef33162a5
RichardChileya <richardchileyawk@gmail.com>
2023-04-19 23:47:19 +0200

    Add order model

--- api/models/order.js
+++ api/models/order.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 // create a table connection
 const orderSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  quantity: Number,
+  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  quantity: { type: Number, default: 1 },
 });
 
 module.exports = mongoose.model('Order', orderSchema);
