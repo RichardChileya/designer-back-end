6ea94596379f98c2b2b5469b0bfd8965cd4a90a1
RichardChileya <richardchileyawk@gmail.com>
2023-04-19 23:46:38 +0200

    Add app file

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ const mongoose = require('mongoose');
 const productsRoutes = require('./api/routes/products.js');
 const orderRoutes = require('./api/routes/orders.js');
 
-mongoose.connect(`mongodb+srv://richardchileyawk:${process.env.MONGO_ATLAS_PW}@designer-back-end.ox332sk.mongodb.net/test`, {
+const userRoutes = require('./api/routes/users.js');
+
+mongoose.connect(`mongodb+srv://richardchileyawk:${process.env.MONGO_ATLAS_PW}@designer-back-end.ox332sk.mongodb.net/designer_app_db`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,7 +25,7 @@ app.use(bodyParser.json());
 // CORS ERROR
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorozation');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     req.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
     return res.status(200).json({});
@@ -32,6 +35,8 @@ app.use((req, res, next) => {
 
 app.use('/products', productsRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
+app.use('/uploads', express.static('uploads'));
 
 // ERROR HANDLING
 app.use((req, res, next) => {
