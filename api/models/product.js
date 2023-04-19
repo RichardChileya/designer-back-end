ab8e68c6b4e03f6435079157cbbcd667206f09e5
RichardChileya <richardchileyawk@gmail.com>
2023-04-19 23:46:15 +0200

    Add product model

--- api/models/product.js
+++ api/models/product.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 // conect table to mongodb
 const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
+  name: { type: String, required: true, maxlength: 100 },
   price: { type: Number, required: true },
+  productImage: {type: String, required: true},
 });
 
 // export schema model
