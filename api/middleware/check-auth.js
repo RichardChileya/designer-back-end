88031bb07738407fc2f6ac00516bfcb62c204e2a
RichardChileya <richardchileyawk@gmail.com>
2023-05-08 16:35:36 +0200

    Add check authentication

---
+++ api/middleware/check-auth.js
@@ -0,0 +1,16 @@
+/* eslint-disable consistent-return */
+/* eslint-disable import/no-extraneous-dependencies */
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]; // extract token from header
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    req.userData = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Auth failed',
+    });
+  }
+};
