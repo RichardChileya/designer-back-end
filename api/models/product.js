8b6c39a2186d1ed7c8827d0e1e14705f5fedc958
RichardChileya <richardchileyawk@gmail.com>
2023-05-08 16:35:50 +0200

    Update products

--- api/models/product.js
+++ api/models/product.js
@@ -5,7 +5,9 @@ const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true, maxlength: 100 },
   price: { type: Number, required: true },
+  description: { type: String, required: true },
   productImage: { type: String, required: true },
+  status: { type: Boolean, required: true, default: 0 },
 });
 
 // export schema model
