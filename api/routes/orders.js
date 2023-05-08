1640e3f87c886bd12fa39ec455be32e7a07f83c4
RichardChileya <richardchileyawk@gmail.com>
2023-05-08 16:36:18 +0200

    Update orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,10 +5,11 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Order = require('../models/order.js');
+const checkAuth = require('../middleware/check-auth.js');
 const product = require('../models/product.js');
 
 // GET ALL METHOD
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
   Order.find()
     .select('_id product quantity')
     .populate('product')
@@ -38,7 +39,7 @@ router.get('/', (req, res, next) => {
 });
 
 // Post METHOD
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
   product.findById(req.body.productId)
     .then((product) => {
       if (!product) {
@@ -77,7 +78,7 @@ router.post('/', (req, res, next) => {
 });
 
 // GET  ID METHOD
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
   Order.findById(req.params.orderId)
     .populate('product')
     .exec()
@@ -112,7 +113,7 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 // DELETE METHOD
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
   Order.deleteOne({ _id: req.params.orderId })
     .exec()
     .then((result) => {
